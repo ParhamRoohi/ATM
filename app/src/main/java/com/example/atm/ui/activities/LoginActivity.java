74f298486369ea9874ddc23ef07444789fce12f8
Parham.Roohi <par81hamroohi@gmail.com>
2025-02-14 00:48:26 +0330

    make login xml and activity file

--- app/src/main/java/com/example/atm/ui/activities/LoginActivity.java
+++ app/src/main/java/com/example/atm/ui/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.atm.ui.activities;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 }
